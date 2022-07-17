0b3a0c73f23043685c223488088a34fde66db808
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2022-07-17 16:48:35 -0300

    Login Logout, create recipe

--- Formacao-Django/aplicacao/usuarios/views.py
+++ Formacao-Django/aplicacao/usuarios/views.py
@@ -1,6 +1,9 @@
 from django.db import reset_queries
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import auth
+from recipes.models import Receita
 # Create your views here.
 def cadastro(request):
     if request.method == 'POST':
@@ -31,11 +34,45 @@ def login(request):
         senha = request.POST['senha']
         if email=="" or senha =="":
             return redirect("login")
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
         return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 def dashboard(request):
-    return render(request, 'usuarios/dashboard.html')
+    if request.user.is_authenticated:
+        receitas = Receita.objects.order_by('-date_receita').filter(pessoa = request.user.id)
+        dados = {
+            'receitas': receitas
+        }
+        return render(request, 'usuarios/dashboard.html', dados)
+    else: 
+        return redirect('index')
+
+def cria_receita(request):
+    if request.method =='POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        user = get_object_or_404(User, pk=request.user.id)
+        
+        receita = Receita.objects.create(pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes, modo_preparo=modo_preparo, rendimento=rendimento, 
+        categoria=categoria, foto_receita=foto_receita, tempo_preparo=tempo_preparo)
+
+        receita.save()
+
+        return redirect('dashboard')
+    else:
+        return render (request, 'usuarios/cria_receita.html')
+        
