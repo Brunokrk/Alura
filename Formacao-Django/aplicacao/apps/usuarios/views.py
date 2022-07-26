7033976099f0e31ba8554d929a14dab0748b5b88
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2022-07-25 21:05:23 -0300

    .

---
+++ Formacao-Django/aplicacao/apps/usuarios/views.py
@@ -0,0 +1,66 @@
+from django.db import reset_queries
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import auth, messages
+from recipes.models import Receita
+# Create your views here.
+def cadastro(request):
+    if request.method == 'POST':
+        nome= request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if campo_vazio(nome):
+            return redirect('cadastro')
+        if campo_vazio(email):
+            return redirect('cadastro')
+        if senha != senha2:
+            messages.error(request, 'As senhas não coincidem!!')
+            return redirect('cadastro')
+        if User.objects.filter(email = email).exists():
+            #já cadastrado
+            return redirect('cadastro')
+        if User.objects.filter(username = nome).exists():
+            return redirect('cadastro')
+
+        user = User.objects.create_user(username=nome, email=email, password = senha)
+        user.save()
+        messages.success(request, 'Cadastro realizado com sucesso!!')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email=="" or senha =="":
+            return redirect("login")
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+        return redirect('dashboard')
+    return render(request, 'usuarios/login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        receitas = Receita.objects.order_by('-date_receita').filter(pessoa = request.user.id)
+        dados = {
+            'receitas': receitas
+        }
+        return render(request, 'usuarios/dashboard.html', dados)
+    else: 
+        return redirect('index')
+
+def campo_vazio(campo):
+    return not campo.strip()
+
+
+
